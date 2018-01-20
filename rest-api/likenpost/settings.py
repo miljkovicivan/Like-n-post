4b739925b28e33d7331d2dff722ac5ccc76dcae9
miljkovicivan <miljkovicivann@gmail.com>
2018-01-21 00:23:27 +0100

    Db host

--- rest-api/likenpost/settings.py
+++ rest-api/likenpost/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': 5432,
     }
 }

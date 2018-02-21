94abcac1f5176b9fc1ed8367a375181c59e83d59
ivan <miljkovicivann@gmail.com>
2018-02-21 17:41:44 +0100

    Added 2fa endpoits

--- rest-api/likenpost/apps/post/views.py
+++ rest-api/likenpost/apps/post/views.py
@@ -105,3 +105,104 @@ class PostViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(queryset, many=True)
 
         return response.Response(serializer.data)
+
+
+
+
+
+
+
+import datetime
+import pyotp
+import pytz
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework.views import APIView
+
+from likenpost.apps.post.models import TwoFA
+
+
+class Enable2FA(APIView):
+
+    def post(self, request, *args, **kwargs):
+
+        secret = pyotp.random_base32()
+
+        totp = pyotp.TOTP(secret)
+
+        if TwoFA.objects.filter(user=request.user).exists():
+            return response.Response({'error': 'Alredy enabled'})
+
+
+        TwoFA.objects.create(user=request.user, secret=secret)
+
+        qr_code = pyotp.totp.TOTP(secret).provisioning_uri(request.data['username'], issuer_name="TradeCore")
+
+        return response.Response({'qr_code': qr_code})
+
+enable_2fa = Enable2FA.as_view()
+
+
+class Disable2FA(APIView):
+
+    def post(self, request, *args, **kwargs):
+
+        if TwoFA.objects.filter(user=request.user).exists():
+            TwoFA.objects.filter(user=request.user).delete()
+            return response.Response({'2fa': 'disabled'})
+        else:
+            return response.Response({'2fa': 'already disabled'})
+
+disable_2fa = Disable2FA.as_view()
+
+
+TIME_FOR_COMPLETION = datetime.timedelta(seconds=60)
+class Complete2FA(APIView):
+
+
+    def post(self, request, *args, **kwargs):
+
+        two_fa = TwoFA.objects.filter(user=request.user).first()  # safe get
+        if two_fa is None:
+            return response.Response({'error': 'Enable 2FA first'})
+
+        now = datetime.datetime.now().replace(tzinfo=pytz.UTC)
+
+        if now - two_fa.created_at > TIME_FOR_COMPLETION:
+            two_fa.delete()
+            return response.Response({'2fa': 'Please enable 2fa again for security reason'})
+
+        totp = pyotp.TOTP(two_fa.secret)
+
+        if totp.verify(request.data['code']):
+            two_fa.completed = True
+            two_fa.save()
+            return response.Response({'2fa': 'completed'})
+        else:
+            return response.Response({'2fa': 'wrong code'})
+
+complete_2fa = Complete2FA.as_view()
+
+
+class Login(APIView):
+
+    permission_classes = (permissions.AllowAny,)
+    def post(self, request, *args, **kwargs):
+
+        user = User.objects.filter(username=request.data['username']).first()
+        two_fa = TwoFA.objects.filter(user=user).first()
+        code = request.data.get('code', None)
+
+        if two_fa:
+            # 2fa is enabled
+            if two_fa.completed:
+                totp = pyotp.TOTP(two_fa.secret)
+                if totp.verify(code):
+                    return obtain_jwt_token(request._request)
+            return response.Response({'error': 'Not ok'})
+        else:
+            return obtain_jwt_token(request._request)
+
+
+login = Login.as_view()
